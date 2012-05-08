59af2c16f05741f2cfe99a9262feb996173e8ac9
Alon Swartz <alon@turnkeylinux.org>
2012-05-08 13:59:30 +0300

    added ajaxplorer (and updated lighttpd-fastcgi rename) in fileserver.mk

--- mk/turnkey/fileserver.mk
+++ mk/turnkey/fileserver.mk
@@ -1,4 +1,4 @@
-COMMON_OVERLAYS += samba-fileserver samba-sid-inithook extplorer
-COMMON_CONF += fileserver-storage samba-rootpass extplorer lighttpd-fastcgi-minimize
+COMMON_OVERLAYS += samba-fileserver samba-sid-inithook ajaxplorer
+COMMON_CONF += fileserver-storage samba-rootpass ajaxplorer lighttpd-fastcgi
 
 include $(FAB_PATH)/common/mk/turnkey/php.mk
