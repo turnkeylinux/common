f34e092999d14692e42b85b97a641b67cf16e93c
Alon Swartz <alon@turnkeylinux.org>
2012-05-08 14:40:32 +0300

    added samba-webmin to configure auto-sync for unix/samba users/groups

--- mk/turnkey/fileserver.mk
+++ mk/turnkey/fileserver.mk
@@ -1,4 +1,4 @@
 COMMON_OVERLAYS += samba-fileserver samba-sid-inithook ajaxplorer
-COMMON_CONF += fileserver-storage samba-rootpass ajaxplorer lighttpd-fastcgi
+COMMON_CONF += fileserver-storage samba-rootpass samba-webmin ajaxplorer lighttpd-fastcgi
 
 include $(FAB_PATH)/common/mk/turnkey/php.mk
