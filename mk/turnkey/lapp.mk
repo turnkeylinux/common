d7e8a23e5f0bb4972397f6333be2a84a23eb7c4d
Alon Swartz <alon@turnkeylinux.org>
2013-04-02 15:59:35 +0300

    added phpsh (interative php type shell, similar to ipython)
    - included in lamp and lapp based appliances

--- mk/turnkey/lapp.mk
+++ mk/turnkey/lapp.mk
@@ -1,7 +1,7 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 443 12320 12321 12322
 
 COMMON_OVERLAYS += phppgadmin-apache confconsole-lapp
-COMMON_CONF += apache-vhost postfix-local phppgadmin phppgadmin-apache
+COMMON_CONF += phpsh apache-vhost postfix-local phppgadmin phppgadmin-apache
 
 include $(FAB_PATH)/common/mk/turnkey/php.mk
 include $(FAB_PATH)/common/mk/turnkey/pgsql.mk
