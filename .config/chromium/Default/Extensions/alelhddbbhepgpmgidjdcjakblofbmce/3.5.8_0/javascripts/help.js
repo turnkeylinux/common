5f28fb9999f512c77573c317dd0c2a88124edb5b
Liraz Siri <liraz@turnkeylinux.org>
2013-12-17 05:49:01 +0200

    disabled Chromium awesome screenshot extension
    rational: not a core part of the experience

--- .config/chromium/Default/Extensions/alelhddbbhepgpmgidjdcjakblofbmce/3.5.8_0/javascripts/help.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by chuang on 11/14/13.
- */
-var a = document.getElementById('ext-page-link');
-a.addEventListener('click',function(){
-    chrome.tabs.create({url:'chrome://extensions/?id=' + chrome.runtime.id});
-});
--- .config/chromium/Default/Extensions/alelhddbbhepgpmgidjdcjakblofbmce/3.5.8_0/javascripts/isload.js
+++
@@ -1,6 +0,0 @@
-﻿var isContentScriptLoaded; 
-if(typeof isContentScriptLoaded == "undefined") {
-	chrome.extension.sendRequest({action:"insert_script"});
-} else {
-	chrome.extension.sendRequest({action:"script_running"});
-}
