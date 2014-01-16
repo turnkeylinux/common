de7341246c62bd6eb661f5c2ed3715df8d24f06c
Alon Swartz <alon@turnkeylinux.org>
2014-01-16 11:38:19 +0200

    added turnkey-desktop.mk (temp hack until we support server/desktop overlays/conf)

---
+++ mk/turnkey-desktop.mk
@@ -0,0 +1,75 @@
+# temp hack until we support server/desktop common overlays and conf
+
+RELEASE ?= debian/$(shell lsb_release -s -c)
+
+CDROOT ?= gfxboot-turnkey
+HOSTNAME ?= $(shell basename $(shell pwd))
+CONF_VARS += HOSTNAME ROOT_PASS NONFREE
+
+COMMON_OVERLAYS := $(COMMON_OVERLAYS)
+COMMON_OVERLAYS += turnkey.d/apt
+COMMON_OVERLAYS += turnkey.d/bashrc
+COMMON_OVERLAYS += turnkey.d/grub
+COMMON_OVERLAYS += turnkey.d/profile
+
+COMMON_CONF := $(COMMON_CONF)
+COMMON_CONF += turnkey.d/apt
+COMMON_CONF += turnkey.d/busybox
+COMMON_CONF += turnkey.d/console-setup
+COMMON_CONF += turnkey.d/cronapt
+COMMON_CONF += turnkey.d/hostname
+COMMON_CONF += turnkey.d/locale
+COMMON_CONF += turnkey.d/motd
+COMMON_CONF += turnkey.d/persistent-net
+COMMON_CONF += turnkey.d/roothome
+COMMON_CONF += turnkey.d/rootpass
+COMMON_CONF += turnkey.d/sysctl
+
+COMMON_REMOVELISTS += turnkey
+
+FAB_SHARE_PATH ?= /usr/share/fab
+
+# below hacks allow inheritors to define their own hooks, which will be
+# prepended. warning: first line *needs* to be empty for this to work
+
+# setup apt and dns for root.build
+define _bootstrap/post
+
+	fab-apply-overlay $(COMMON_OVERLAYS_PATH)/turnkey.d/apt $O/bootstrap;
+	fab-chroot $O/bootstrap --script $(COMMON_CONF_PATH)/turnkey.d/apt;
+	fab-chroot $O/bootstrap "echo nameserver 8.8.8.8 > /etc/resolv.conf";
+	fab-chroot $O/bootstrap "echo nameserver 8.8.4.4 >> /etc/resolv.conf";
+endef
+bootstrap/post += $(_bootstrap/post)
+
+# tag package management system with release package
+# set /etc/turnkey_version and apt user-agent
+define _root.patched/post
+	
+	# 
+	# tagging package management system with release package
+	# setting /etc/turnkey_version and apt user-agent
+	#
+	$(FAB_SHARE_PATH)/make-release-deb.py $(FAB_PATH)/products/core/changelog $O/root.patched
+	@if [ -f ./changelog ]; then \
+		echo $(FAB_SHARE_PATH)/make-release-deb.py ./changelog $O/root.patched; \
+		$(FAB_SHARE_PATH)/make-release-deb.py ./changelog $O/root.patched; \
+		turnkey_version=$$($(FAB_SHARE_PATH)/turnkey-version.py --dist=$(CODENAME) --tag=$(VERSION_TAG) ./changelog $(FAB_ARCH)); \
+		turnkey_aptconf="Acquire::http::User-Agent \"TurnKey APT-HTTP/1.3 ($$turnkey_version)\";"; \
+		echo $$turnkey_version > $O/root.patched/etc/turnkey_version; \
+		echo $$turnkey_aptconf > $O/root.patched/etc/apt/apt.conf.d/01turnkey; \
+	else \
+		echo; \
+		echo "WARNING: can't tag local release (./changelog doesn't exist)"; \
+		echo; \
+	fi
+
+	fab-chroot $O/root.patched "dpkg -i *.deb && rm *.deb && rm -f /var/log/dpkg.log"
+
+	fab-chroot $O/root.patched "which insserv >/dev/null && insserv"
+	fab-chroot $O/root.patched "which postsuper >/dev/null && postsuper -d ALL || true"
+endef
+root.patched/post += $(_root.patched/post)
+
+include $(FAB_SHARE_PATH)/product.mk
+
