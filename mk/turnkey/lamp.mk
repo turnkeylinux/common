82daa83de4d96a694eeb3459ad444f94f66fe1b1
Jeremy Davis <jeremy@turnkeylinux.org>
2015-05-16 05:35:32 +0000

    update make file

--- mk/turnkey/lamp.mk
+++ mk/turnkey/lamp.mk
@@ -1,7 +1,7 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 443 12320 12321 12322
 
-COMMON_OVERLAYS += apache phpmyadmin phpmyadmin-apache confconsole-lamp
-COMMON_CONF += phpsh apache-vhost postfix-local phpmyadmin phpmyadmin-apache
+COMMON_OVERLAYS += apache adminer adminer-apache confconsole-lamp
+COMMON_CONF += phpsh apache-vhost postfix-local adminer adminer-apache
 
 include $(FAB_PATH)/common/mk/turnkey/php.mk
 include $(FAB_PATH)/common/mk/turnkey/mysql.mk
