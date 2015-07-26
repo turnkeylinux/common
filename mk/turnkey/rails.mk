da2f745841ab6e8e3814b06bd98214690e69b86d
Dude4Linux <dude4linux@gmail.com>
2015-07-26 03:33:49 +0000

    set apache2 production security

--- mk/turnkey/rails.mk
+++ mk/turnkey/rails.mk
@@ -1,4 +1,4 @@
 COMMON_OVERLAYS += rails apache
-COMMON_CONF += postfix-local rails apache-vhost
+COMMON_CONF += postfix-local rails apache-vhost apache-security
 
 include $(FAB_PATH)/common/mk/turnkey/mysql.mk
