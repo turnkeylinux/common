5de4495ea32fbe7ef61f16ea98938740c3d2d61c
Anton Pyrogovskyi <q@dae.pp.ua>
2015-08-03 14:52:09 +0000

    Update File Server Makefile

--- mk/turnkey/fileserver.mk
+++ mk/turnkey/fileserver.mk
@@ -1,4 +1,4 @@
-COMMON_OVERLAYS += samba-fileserver samba-sid-inithook ajaxplorer
-COMMON_CONF += fileserver-storage samba-rootpass samba-webmin ajaxplorer lighttpd-fastcgi
+COMMON_OVERLAYS += samba-fileserver samba-sid-inithook samba-dav nfs tkl-webcp nginx
+COMMON_CONF += fileserver-storage samba-rootpass samba-webmin samba-dav nfs tkl-webcp
 
 include $(FAB_PATH)/common/mk/turnkey/php.mk
