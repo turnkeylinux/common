f8bdb8500e3ec688ee1a34cdc0f03d21d0b3f54c
Dude4Linux <dude4linux@gmail.com>
2015-08-04 14:43:06 +0000

    move apache-security to security, run on all appliances

--- mk/turnkey/lamp.mk
+++ mk/turnkey/lamp.mk
@@ -1,7 +1,7 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 443 12320 12321 12322
 
 COMMON_OVERLAYS += apache adminer confconsole-lamp
-COMMON_CONF += phpsh apache-vhost apache-security adminer-apache adminer-mysql
+COMMON_CONF += phpsh apache-vhost adminer-apache adminer-mysql
 
 include $(FAB_PATH)/common/mk/turnkey/php.mk
 include $(FAB_PATH)/common/mk/turnkey/mysql.mk
