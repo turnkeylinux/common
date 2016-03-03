99a251071b8e5b5eefe4f278b009be7144fe4a82
Anton Pyrogovskyi <anton@turnkeylinux.org>
2016-03-03 17:08:44 +0000

    Update fileserver makefile

--- mk/turnkey/fileserver.mk
+++ mk/turnkey/fileserver.mk
@@ -1,4 +1,2 @@
-COMMON_OVERLAYS += samba-fileserver samba-sid-inithook samba-dav nfs tkl-webcp nginx
-COMMON_CONF += fileserver-storage samba-rootpass samba-webmin samba-dav nfs tkl-webcp composer
-
-include $(FAB_PATH)/common/mk/turnkey/php.mk
+COMMON_OVERLAYS += apache samba-fileserver samba-sid-inithook samba-dav nfs tkl-webcp
+COMMON_CONF += apache-vhost fileserver-storage samba-rootpass samba-webmin samba-dav nfs tkl-webcp
