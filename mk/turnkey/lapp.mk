b3a20f9eb825c21643959dc58bfc8136e362e63f
Jeremy Davis <jeremy@turnkeylinux.org>
2018-02-26 18:03:59 +1100

    remove phpsh from default lamp/lapp appliances

--- mk/turnkey/lapp.mk
+++ mk/turnkey/lapp.mk
@@ -1,7 +1,7 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 443 12320 12321 12322
 
 COMMON_OVERLAYS += apache adminer confconsole-lapp
-COMMON_CONF += phpsh apache-vhost apache-cgi adminer-apache adminer-pgsql composer
+COMMON_CONF += apache-vhost apache-cgi adminer-apache adminer-pgsql composer
 
 include $(FAB_PATH)/common/mk/turnkey/php.mk
 include $(FAB_PATH)/common/mk/turnkey/pgsql.mk
