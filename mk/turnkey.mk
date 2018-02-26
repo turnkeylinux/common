74fcbb1632032ee0dd3962da32c225459a4090b8
Jeremy Davis <jeremy@turnkeylinux.org>
2018-02-26 15:04:45 +1100

    update mk/turnkey.mk to support PHP56 var

--- mk/turnkey.mk
+++ mk/turnkey.mk
@@ -3,7 +3,7 @@ RELEASE ?= debian/$(shell lsb_release -s -c)
 CDROOT ?= gfxboot-turnkey
 HOSTNAME ?= $(shell basename $(shell pwd))
 
-CONF_VARS += HOSTNAME ROOT_PASS NONFREE
+CONF_VARS += HOSTNAME ROOT_PASS NONFREE PHP56
 CONF_VARS += WEBMIN_THEME WEBMIN_FW_TCP_INCOMING WEBMIN_FW_TCP_INCOMING_REJECT WEBMIN_FW_UDP_INCOMING WEBMIN_FW_NAT_EXTRA WEBMIN_FW_MANGLE_EXTRA
 # these are needed to control styling of credits (e.g., conf/apache-credit)
 CONF_VARS += CREDIT_STYLE CREDIT_STYLE_EXTRA CREDIT_ANCHORTEXT CREDIT_LOCATION
@@ -15,16 +15,22 @@ COMMON_REMOVELISTS_FINAL += turnkey
 
 FAB_SHARE_PATH ?= /usr/share/fab
 
+APT_OVERLAY = fab-apply-overlay $(COMMON_OVERLAYS_PATH)/turnkey.d/apt $O/bootstrap;
+
+ifdef PHP56
+APT_OVERLAY += fab-apply-overlay $(COMMON_OVERLAYS_PATH)/php-alt $O/bootstrap;
+endif
+
 # below hacks allow inheritors to define their own hooks, which will be
 # prepended. warning: first line *needs* to be empty for this to work
 
 # setup apt and dns for root.build
 define _bootstrap/post
 
-	fab-apply-overlay $(COMMON_OVERLAYS_PATH)/turnkey.d/apt $O/bootstrap;
-	fab-chroot $O/bootstrap --script $(COMMON_CONF_PATH)/turnkey.d/apt;
+	$(APT_OVERLAY)
 	fab-chroot $O/bootstrap "echo nameserver 8.8.8.8 > /etc/resolv.conf";
 	fab-chroot $O/bootstrap "echo nameserver 8.8.4.4 >> /etc/resolv.conf";
+	fab-chroot $O/bootstrap --script $(COMMON_CONF_PATH)/turnkey.d/apt;
 endef
 bootstrap/post += $(_bootstrap/post)
 
