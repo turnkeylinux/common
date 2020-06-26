e987029590d74d4d5770a678e0801aeefdda6142
Jeremy Davis <jeremy@turnkeylinux.org>
2020-06-26 13:10:46 +1000

    Tomcat inithook minor refactoring:
    - Remove redundant TOMCAT_INIT var.
    - Set TOMCAT as tomcat version (i.e. 'tomcat9' this release).
    - Leverage use of TOMCAT throughout script; used via fstrings.
    - Make code PEP8 compliant.

--- overlays/tomcat/usr/lib/inithooks/bin/tomcat.py
+++ overlays/tomcat/usr/lib/inithooks/bin/tomcat.py
@@ -10,13 +10,12 @@ import sys
 import getopt
 import subprocess
 import xml.dom.minidom
+import signal
 
 from dialog_wrapper import Dialog
 
-import signal
+TOMCAT = "tomcat9"
 
-TOMCAT_INIT="/etc/init.d/tomcat9"
-TOMCAT_USERS="/etc/tomcat9/tomcat-users.xml"
 
 def usage(s=None):
     if s:
@@ -24,6 +23,7 @@ def usage(s=None):
     print("Syntax: %s [options]" % sys.argv[0], file=sys.stderr)
     print(__doc__, file=sys.stderr)
     sys.exit(1)
+
 
 def main():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
@@ -45,24 +45,25 @@ def main():
             "Tomcat Password",
             "Enter new password for the Tomcat 'admin' account.")
 
-
-    doc = xml.dom.minidom.parse(TOMCAT_USERS).documentElement
-    users = doc.getElementsByTagName('user')
+    tomcat_user_conf = f"/etc/{TOMCAT}/tomcat-users.xml"
+    doc = xml.dom.minidom.parse(tomcat_user_conf).documentElement
+    users = doc.getElementsByTagName('user')1
     for user in users:
         if not user.getAttribute('username') == 'admin':
             continue
         user.setAttribute('password', password)
 
-    with open(TOMCAT_USERS, 'w') as fob:
+    with open(tomcat_user_conf, 'w') as fob:
         fob.write("<?xml version='1.0' encoding='utf-8'?>\n")
         doc.writexml(fob)
         fob.write("\n")
 
     # restart tomcat if running so password change will take effect
-    tomcat_status = ['systemctl', '-q', 'is-active', 'tomcat9.service']
+    tomcat_status = ['systemctl', '-q', 'is-active', f'{TOMCAT}.service']
     tomcat_stopped = subprocess.run(tomcat_status).returncode
     if not tomcat_stopped:
-         subprocess.run(['systemctl', 'restart', 'tomcat9.service'])
+        subprocess.run(['systemctl', 'restart', f'{TOMCAT}.service'])
+
 
 if __name__ == "__main__":
     main()
