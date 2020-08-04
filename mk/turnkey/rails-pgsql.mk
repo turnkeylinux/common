e33295f557bf41a93c44e853ae0e0ca2a4cf9963
Jeremy Davis <jeremy@turnkeylinux.org>
2020-08-04 14:36:49 +1000

    Remove yarn from common rails-pgsql conf/overlay and include via rails-pgsql make file.

--- mk/turnkey/rails-pgsql.mk
+++ mk/turnkey/rails-pgsql.mk
@@ -2,5 +2,5 @@
 include $(FAB_PATH)/common/mk/turnkey/pgsql.mk
 
 # it is also important that rails goes before rails-pgsql
-COMMON_OVERLAYS += rails rails-pgsql apache
-COMMON_CONF += rails-pgsql apache-vhost
+COMMON_OVERLAYS += yarn rails rails-pgsql apache
+COMMON_CONF += yarn rails-pgsql apache-vhost
