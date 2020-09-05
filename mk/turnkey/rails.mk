feeb9d3d13556f3ce3a81f77070346591e7583df
qq7 <qq7dev@gmail.com>
2020-09-05 11:36:22 +0000

    remove apache-credit from rails

--- mk/turnkey/rails.mk
+++ mk/turnkey/rails.mk
@@ -1,4 +1,4 @@
 COMMON_OVERLAYS += yarn rails apache 
-COMMON_CONF += yarn rails apache-vhost apache-credit 
+COMMON_CONF += yarn rails apache-vhost 
 
 include $(FAB_PATH)/common/mk/turnkey/mysql.mk
