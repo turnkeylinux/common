806dcb4be24b66fe1ebbc13453a8c317cf21a6e5
Jeremy Davis <jeremy@turnkeylinux.org>
2022-01-18 04:33:15 +0000

    Replace adhoc apache inclusion and instead use dedicated mk file.

--- mk/turnkey/lapp.mk
+++ mk/turnkey/lapp.mk
@@ -1,7 +1,8 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 443 12320 12321 12322
 
-COMMON_OVERLAYS += apache adminer confconsole-lapp
-COMMON_CONF += apache-vhost apache-cgi adminer-apache adminer-pgsql
+COMMON_OVERLAYS += adminer confconsole-lapp
+COMMON_CONF += apache-cgi adminer-apache adminer-pgsql
 
+include $(FAB_PATH)/common/mk/turnkey/apache.mk
 include $(FAB_PATH)/common/mk/turnkey/php.mk
 include $(FAB_PATH)/common/mk/turnkey/pgsql.mk
--- mk/turnkey/web2py.mk
+++ mk/turnkey/web2py.mk
@@ -1,6 +1,7 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 443 12320 12321
 
-COMMON_OVERLAYS += web2py apache github-latest-release
-COMMON_CONF += web2py apache-vhost
+COMMON_OVERLAYS += web2py github-latest-release
+COMMON_CONF += web2py
 
+include $(FAB_PATH)/common/mk/turnkey/apache.mk
 include $(FAB_PATH)/common/mk/turnkey/mysql.mk
