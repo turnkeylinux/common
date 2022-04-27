2a928d86a750c28d460a5b05ed0ccbbd7e8c3dc2
Jeremy Davis <jeremy@turnkeylinux.org>
2022-04-27 04:07:56 +0000

    Update nodejs.mk

--- mk/turnkey/nodejs.mk
+++ mk/turnkey/nodejs.mk
@@ -1,6 +1,6 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 443 12320 12321
 
 COMMON_OVERLAYS += nodejs
-COMMON_CONF += nodejs-install nodejs
+COMMON_CONF += nodejs
 
 include $(FAB_PATH)/common/mk/turnkey/nginx.mk
