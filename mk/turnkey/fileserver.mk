243540ba8ead9503fdc229bd7839c0815f7c84b5
Jeremy Davis <jeremy@turnkeylinux.org>
2024-06-07 06:42:08 +0000

    Add Avahi port to firewall

--- mk/turnkey/fileserver.mk
+++ mk/turnkey/fileserver.mk
@@ -1,5 +1,5 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 135 139 443 445 5357 12321
-WEBMIN_FW_UDP_INCOMING = 137 138 3702
+WEBMIN_FW_UDP_INCOMING = 137 138 3702 5353
 
 COMMON_OVERLAYS += samba-fileserver samba-sid-inithook samba-dav nfs
 COMMON_CONF += fileserver-storage samba-rootpass samba-webmin samba-dav nfs
