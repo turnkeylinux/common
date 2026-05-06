175803c78157e62a60f8deb5c9b6499cd9d61c19
Jeremy Davis <jeremy@turnkeylinux.org>
2026-05-07 06:44:10 +1000

    Retab TKL Adminer customized components

--- overlays/adminer/usr/share/adminer/adminer/tkl-index.php
+++ overlays/adminer/usr/share/adminer/adminer/tkl-index.php
@@ -7,27 +7,27 @@
 */
 
 function adminer_object() {
-        // required to run any plugin
-        include_once "../plugins/plugin.php";
+    // required to run any plugin
+    include_once "../plugins/plugin.php";
 
-        // autoloader
-        foreach (glob("../plugins/*.php") as $filename) {
-                include_once $filename;
-        }
+    // autoloader
+    foreach (glob("../plugins/*.php") as $filename) {
+        include_once $filename;
+    }
 
-        // Array of servers
-        // We will use the below to customize this file later with conf script
-        $tkl_servers = array("localhost" => "DB_DESC on " . gethostname());
-        $tkl_database = "DRIVER";
+    // Array of servers
+    // We will use the below to customize this file later with conf script
+    $tkl_servers = array("localhost" => "DB_DESC on " . gethostname());
+    $tkl_database = "DRIVER";
 
-        // Plugins we want to use, disable version checking and lock server to
-        // localhost and database type
-        $plugins = array(
-                new AdminerVersionNoverify,
-                new TurnKeyAdminerLoginServers($tkl_servers, $tkl_database )                
-        );
+    // Plugins we want to use, disable version checking and lock server to
+    // localhost and database type
+    $plugins = array(
+        new AdminerVersionNoverify,
+        new TurnKeyAdminerLoginServers($tkl_servers, $tkl_database)
+    );
 
-        return new AdminerPlugin($plugins);
+    return new AdminerPlugin($plugins);
 }
 
 // include original Adminer
