6f51a44af221a4602c00222bde2295ca1bedd248
Jeremy Davis <jeremy@turnkeylinux.org>
2026-05-07 11:15:55 +1000

    Add new adminer conf script & mk file; update other files to use them

--- mk/turnkey/lapp.mk
+++ mk/turnkey/lapp.mk
@@ -1,8 +1,9 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 443 12321 12322
 
-COMMON_OVERLAYS += adminer confconsole-lapp
+COMMON_OVERLAYS += confconsole-lapp
 COMMON_CONF += apache-cgi adminer-apache adminer-pgsql
 
 include $(FAB_PATH)/common/mk/turnkey/apache.mk
 include $(FAB_PATH)/common/mk/turnkey/php.mk
 include $(FAB_PATH)/common/mk/turnkey/pgsql.mk
+include $(FAB_PATH)/common/mk/turnkey/adminer.mk
--- mk/turnkey/lighttpd-php-mysql.mk
+++ mk/turnkey/lighttpd-php-mysql.mk
@@ -1,8 +1,9 @@
 WEBMIN_FW_TCP_INCOMING = 22 80 443 12321 12322
 
-COMMON_OVERLAYS += adminer confconsole-lamp
+COMMON_OVERLAYS += confconsole-lamp
 COMMON_CONF += adminer-lighttpd adminer-mysql
 
 include $(FAB_PATH)/common/mk/turnkey/lighttpd.mk
 include $(FAB_PATH)/common/mk/turnkey/mysql.mk
 include $(FAB_PATH)/common/mk/turnkey/php-fpm.mk
+include $(FAB_PATH)/common/mk/turnkey/adminer.mk
